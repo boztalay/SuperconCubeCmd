e65e4b301850ccd984091f89c12da028f7fa2590
Ben Oztalay <boztalay@apple.com>
2018-11-02 11:42:16 -0700

    Object orientation

--- cube.py
+++ cube.py
@@ -2,35 +2,36 @@ import cmd
 import serial
 import sys
 
-class Cube:
+class Cube(object):
+    def __init__(self, serialPort):
+        self.port = serial.Serial(serialPort, 115200)
+
+    def sendCommand(self, command):
+        self.port.write(command + "\r")
+        return self.readResponse()
+
+    def readResponse(self):
+        response = ""
+
+        while not response.endswith("\n+"):
+            character = self.port.read(1)
+            if len(character) == 0:
+                print "Whoops, couldn't read a response"
+                sys.exit(1)
+
+            response += character
+
+            if response.endswith("error"):
+                print "Got an error!"
+                self.port.flush()
+                return response
+
+        self.port.flush()
+        return response[:-2].strip()
 
 def main(serialPort):
-    port = serial.Serial(serialPort, 115200)
-
-    for page in range(0, 65536):
-        sendCommand(port, "w " + str(page))
-        sendCommand(port, "m b f")
-        print page
-
-def sendCommand(port, command):
-    port.write(command + "\r")
-    return readToPlus(port)
-
-def readToPlus(port):
-    response = ""
-
-    while not response.endswith("\n+"):
-        character = port.read(1)
-        if len(character) == 0:
-            print "Whoops, couldn't read a response"
-            sys.exit(1)
-
-        response += character
-
-        if response.endswith("error"):
-            print "Got an error!"
-
-    return response[:-2].strip()
+    cube = Cube(serialPort)
+    print cube.sendCommand("m b u")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
